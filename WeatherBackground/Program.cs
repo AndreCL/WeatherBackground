f1e9ea4ba70bde341f65456c6c67379a765b0177
Andre CL <admuk0je@intra.coop>
2017-04-04 14:34:53 +0200

    Code

---
+++ WeatherBackground/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherBackground
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            int result = Wallpaper.Set(new Uri("http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by=2500&mode=long"), Wallpaper.Style.Centered);
+
+            return result;
+        }
+    }
+}
---
+++ WeatherBackground/Wallpaper.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Net;
+using System.Runtime.InteropServices;
+
+public sealed class Wallpaper
+{
+    Wallpaper() { }
+
+    const int SPI_SETDESKWALLPAPER = 20;
+    const int SPIF_UPDATEINIFILE = 0x01;
+    const int SPIF_SENDWININICHANGE = 0x02;
+
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+    public enum Style : int
+    {
+        Tiled,
+        Centered,
+        Stretched
+    }
+
+    public static int Set(Uri uri, Style style)
+    {
+        Stream s;
+
+        try
+        {
+            s = new WebClient().OpenRead(uri.ToString());
+        }
+        catch (WebException) //if no internet connection
+        {
+            return -1;
+        }
+        
+
+        System.Drawing.Image img = System.Drawing.Image.FromStream(s);
+        string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+        img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
+
+        RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+        if (style == Style.Stretched) //not tested
+        {
+            key.SetValue(@"WallpaperStyle", 2.ToString());
+            key.SetValue(@"TileWallpaper", 0.ToString());
+        }
+
+        if (style == Style.Centered) //tested
+        {
+            key.SetValue(@"WallpaperStyle", 0.ToString());
+            key.SetValue(@"TileWallpaper", 0.ToString());
+        }
+
+        if (style == Style.Tiled) //not tested
+        {
+            key.SetValue(@"WallpaperStyle", 1.ToString());
+            key.SetValue(@"TileWallpaper", 1.ToString());
+        }
+
+        SystemParametersInfo(SPI_SETDESKWALLPAPER,
+            0,
+            tempPath,
+            SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+
+        return 0;
+    }
+}
