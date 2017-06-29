c82a90fc68fd4631b917bc196293c62406222e4e
Andre CL <admuk0je@intra.coop>
2017-06-29 14:42:06 +0200

    Added it to a background of my hometown

--- WeatherBackground/Program.cs
+++ WeatherBackground/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Configuration;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
 
 namespace WeatherBackground
 {
@@ -14,16 +18,69 @@ namespace WeatherBackground
 
             int result = -3;
 
-            if (ConfigurationManager.AppSettings["CountryCode"] == "DK")
+            Image backImg = Image.FromFile("background.jpg");
+            Image mrkImg = null; //= Image.FromFile("watermark.png");
+            Graphics g = Graphics.FromImage(backImg);
+
+            try
             {
-                result = Wallpaper.Set(new Uri($"http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by={ZipCode}&mode=long"),Wallpaper.Style.Centered);
+                if (ConfigurationManager.AppSettings["CountryCode"] == "DK")
+                {
+                    //result = Wallpaper.Set(
+                    //    new Uri($"http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by={ZipCode}&mode=long"),
+                    //    Wallpaper.Style.Centered);
+                    WebClient client = new WebClient();
+                    Stream stream = client.OpenRead($"http://servlet.dmi.dk/byvejr/servlet/byvejr_dag1?by={ZipCode}&mode=long");
+
+                    if (stream != null)
+                    {
+                        mrkImg = Image.FromStream(stream);
+                        stream.Flush();
+                        stream.Close();
+                    }
+                    client.Dispose();
+                }
+                else
+                {
+                    //result = Wallpaper.Set(
+                    //    new Uri($"http://servlet.dmi.dk/byvejr/servlet/world_image?city={ZipCode}&mode=dag1_2"),
+                    //    Wallpaper.Style.Centered);
+                    WebClient client = new WebClient();
+                    Stream stream = client.OpenRead($"http://servlet.dmi.dk/byvejr/servlet/world_image?city={ZipCode}&mode=dag1_2");
+
+                    if (stream != null)
+                    {
+                        mrkImg = Image.FromStream(stream);
+                        stream.Flush();
+                        stream.Close();
+                    }
+                    client.Dispose();
+                }
+                g.DrawImage(mrkImg, backImg.Width-(mrkImg.Width/2) / 2, backImg.Height- (mrkImg.Height / 2) / 2);
+                Wallpaper.Set(backImg, Wallpaper.Style.Centered);
+                //backImg.Save("result.jpg");
             }
-            else
+            catch (Exception e)
             {
-                result = Wallpaper.Set(new Uri($"http://servlet.dmi.dk/byvejr/servlet/world_image?city={ZipCode}&mode=dag1_2"), Wallpaper.Style.Centered);
+                Wallpaper.Set(backImg, Wallpaper.Style.Centered);
             }
+           
 
             return result;
+        }
+
+        public static Bitmap SaveImage(string imageUrl)
+        {
+
+            WebClient client = new WebClient();
+            Stream stream = client.OpenRead(imageUrl);
+            Bitmap bitmap = new Bitmap(stream);
+
+            stream.Flush();
+            stream.Close();
+            client.Dispose();
+
+            return bitmap;
         }
     }
 }
--- WeatherBackground/Wallpaper.cs
+++ WeatherBackground/Wallpaper.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32;
 using System;
+using System.Drawing;
 using System.IO;
 using System.Net;
 using System.Runtime.InteropServices;
@@ -19,7 +20,8 @@ public sealed class Wallpaper
     {
         Tiled,
         Centered,
-        Stretched
+        Stretched,
+        Span
     }
 
     public static int Set(Uri uri, Style style)
@@ -40,7 +42,39 @@ public sealed class Wallpaper
         }
 
 
-        System.Drawing.Image img = System.Drawing.Image.FromStream(s);
+        Image img = Image.FromStream(s);
+        string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
+        img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
+
+        RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Control Panel\Desktop", true);
+        if (style == Style.Stretched) //not tested
+        {
+            key.SetValue(@"WallpaperStyle", 2.ToString());
+            key.SetValue(@"TileWallpaper", 0.ToString());
+        }
+
+        if (style == Style.Centered) //tested
+        {
+            key.SetValue(@"WallpaperStyle", 0.ToString());
+            key.SetValue(@"TileWallpaper", 0.ToString());
+        }
+
+        if (style == Style.Tiled) //not tested
+        {
+            key.SetValue(@"WallpaperStyle", 1.ToString());
+            key.SetValue(@"TileWallpaper", 1.ToString());
+        }
+
+        SystemParametersInfo(SPI_SETDESKWALLPAPER,
+            0,
+            tempPath,
+            SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+
+        return 0;
+    }
+
+    public static int Set(Image img, Style style)
+    {
         string tempPath = Path.Combine(Path.GetTempPath(), "wallpaper.bmp");
         img.Save(tempPath, System.Drawing.Imaging.ImageFormat.Bmp);
 
